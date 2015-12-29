4a23ba3a8760a6221be3af3eafa313c040e1a69d
TheLazyBastard <ngiovi@hotmail.com>
2015-12-29 05:04:39 -0300

    Faltaria Realizar pruebas con una placa pinguino, pero ya estaria terminada la funcionalidad, por ultimo solo restaria debugear

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from PySide.QtGui import *
 import sys
 import UI
 from Services import SerialService
+
 class MainWindow(QDialog,UI.Ui_Form):
     def __init__(self, parent = None):
         super(MainWindow,self).__init__(parent)
@@ -27,22 +28,44 @@ class MainWindow(QDialog,UI.Ui_Form):
         COM = str(self.CboCOMPorts.currentText())
         BR = int(self.CboBaudRate.currentText())
         self.Servicio.Conectar(COM,BR)
+        self.HiloLectura = ThreadLectura(self.Servicio.GetConexion())
+        self.connect(self.HiloLectura,SIGNAL("LEER_DATOS"), self.ActualizarTxtRx,Qt.DirectConnection)
+        if self.Servicio.conexion.isOpen():
+            self.HiloLectura.start()
+            print(self.HiloLectura)
 
     def Desconectar(self):
-        self.Servicio.Desconectar()
+        if self.HiloLectura.isRunning():
+            self.Servicio.Desconectar()
+            self.HiloLectura.quit()
 
     def EnviarDatos(self):
-        if self.Servicio.conexion.isOpen():
-            Texto = self.TxtEnviar.text()
-            Enviar = []
-            for char in Texto:
-                Enviar.append(str(char))
-            if self.ChkNuevaLinea.checkState():
-                Enviar.append('\n')
-            if self.ChkRetornoCarro.checkState():
-                Enviar.append('\r')
-            self.Servicio.Enviar(''.join(Enviar))
-            self.TxtEnviar.setText("")
+        Texto = self.TxtEnviar.text()
+        Enviar = []
+        for char in Texto:
+            Enviar.append(str(char))
+        if self.ChkNuevaLinea.checkState():
+            Enviar.append('\n')
+        if self.ChkRetornoCarro.checkState():
+            Enviar.append('\r')
+        self.Servicio.Enviar(''.join(Enviar))
+        self.TxtEnviar.setText("")
+
+    def ActualizarTxtRx(self,val):
+        print val
+        self.TxtDatosRecibidos.insertPlainText(str(val))
+
+class ThreadLectura(QThread):
+
+    def __init__(self,Conexion,parent = None):
+        super(ThreadLectura,self).__init__(parent)
+        self.conexion = Conexion
+
+    def run(self):
+        while self.conexion.isOpen():
+            var = self.conexion.read()
+            self.emit(SIGNAL("LEER_DATOS(Qstring)"),var)
+            #time.sleep(0.1)
 
 app = QApplication(sys.argv)
 form = MainWindow()
